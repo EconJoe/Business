cdc4b2ceade885c354ffde8dc9bf5adbdd7581c8
EconJoe <staudt.joe@gmail.com>
2016-12-04 05:58:42 -0500

    Add files via upload

--- bng.pl
+++ bng.pl
@@ -7,6 +7,7 @@ use LWP::Simple;
 my $path = "B:\\Business\\Data\\BabyNameGuesser";
 open (INFILE, "<$path\\genni_firstnamelist.txt") or die "Can't open subjects file: genni_firstnamelist.txt";
 open (OUTFILE, ">$path\\BNG_Harvest.txt") or die "Can't open subjects file: BNG_Harvest.txt";
+print OUTFILE "first_name	gender\n";
 
 while (<INFILE>) {
   
@@ -17,11 +18,11 @@ while (<INFILE>) {
     my $url = "http://www.gpeters.com/names/baby-names.php?name=$name&button=Go";
     my $content = get $url;
     die "Couldn't get $url" unless defined $content;
-    sleep 1.5;
+    sleep 1;
 
     if ($content=~/<b>It's a (.*?)!<\/b>/) {
        my $gender=$1;
-       print OUTFILE "$name 	$gender\n";
+       print OUTFILE "$name	$gender\n";
     }
   }
 }
